fd19f9503e600382219b7f026654f243fb8d7859
Noa <noapaul.gratacos@orange.fr>
2022-05-05 14:33:58 +0200

    Add close function
    Function to close the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+modalClose.forEach((close) => close.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+function closeModal() {
+  modalbg.style.display = "none";
+}
